803fed57d34eac1ebecd5bdee50c356a1a6ea199
vedanshagrahari217 <vedanshagrahari217@gmail.com>
2026-03-10 15:30:01 +0530

    Uc2 completed

---
+++ Stepclasses/OOPSBannerApp.java
@@ -0,0 +1,21 @@
+/**
+ * OOPS Banner App
+ *
+ * UC2: Render OOPS as Banner using '*' and spaces
+ *
+ * @author VEDANSH
+ * @version 2.0
+ */
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("  **     **     **     *** ");
+        System.out.println(" *     *   *     *   *      *   *     ");
+        System.out.println(" *     *   *     *   *      *   *     ");
+        System.out.println(" *     *   *     *   **      *** ");
+        System.out.println(" *     *   *     *   *                * ");
+        System.out.println(" *     *   *     *   *                * ");
+        System.out.println("  **     **    *           *** " );
+    }
+}
